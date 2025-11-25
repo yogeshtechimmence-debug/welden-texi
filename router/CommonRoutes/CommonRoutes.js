9a5df844b41635d0490c67d26400236ed1456fd1
yogeshtechimmence-debug <yogesh.techimmence@gmail.com>
2025-11-25 17:40:51 +0530

    add login routes

--- router/CommonRoutes/CommonRoutes.js
+++ router/CommonRoutes/CommonRoutes.js
@@ -1,9 +1,10 @@
 // routes/chatRoutes.js
 import express from "express";
-import { UserSignUp } from "../../controller/CommonFolder/UserController.js";
+import { UserLogin, UserSignUp } from "../../controller/CommonFolder/UserController.js";
 
 const router = express.Router();
 
 router.post("/signup", UserSignUp)
+router.post("/login", UserLogin)
 
 export default router;
